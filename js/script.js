4ac9c97e7b27011d881604e3dcc28af05d818ebd
Rontab <rontab95@yahoo.com>
2022-01-29 06:34:14 -0800

    code finishing touches

--- js/script.js
+++ js/script.js
@@ -1,115 +1,127 @@
 //variables used in this project
-const divOverview = document.querySelector(".overview");
+const overView = document.querySelector(".overview");
 const info = document.querySelector(".user-info");
 const username = "rontab95";
-const dispRepoList = document.querySelector(".repo-list"); //<==repos list 
-const reposSection = document.querySelector("repos");
-const repoData = document.querySelector("repo-data");
+const repoList = document.querySelector(".repo-list"); //<==repos list 
+// where all the repo information appears 
+const reposSection = document.querySelector(".repos");
+// where the individual repo data will appear
+const repoData = document.querySelector(".repo-data");
+viewRepoButton = document.querySelector(".view-repos")
+//filter-repos class
+filterRepos = document.querySelector(".filter-repos");
 
-//API function to fetch my GitHub data
-const getGit = async function () {
+//API function to fetch API JSON Data
+const getRepos = async function () {
     const userRequest = await fetch(`https://api.github.com/users/${username}`);
-    const data = await userRequest.json();
-    userInfo(data);
+    const jsonData = await userRequest.json();
+    userInfo(jsonData);
 };
 // Callback to function 
-getGit();
+getRepos();
 //Function to append data to the html 
-const userInfo = function (json) {
+const userInfo = function (jsonData) {
 
     let div = document.createElement("div");
-    div.classList.add("user-info");
-
-    div.innerHTML =
+    div.classList.add("user-info"); //<==add user-info class
+    div.innerHTML = //<==5 placeholders
         `<figure>
-      <img alt="user avatar" src=${json.avatar_url} />
+      <img alt="user avatar" src=${jsonData.avatar_url} />
     </figure>
     <div>
-      <p><strong>Name:</strong> ${json.name}</p>
-      <p><strong>Bio:</strong> ${json.bio}</p>
-      <p><strong>Location:</strong> ${json.location}</p>
-      <p><strong>Number of public repos:</strong> ${json.public_repos}</p>
+      <p><strong>Name:</strong> ${jsonData.name}</p>
+      <p><strong>Bio:</strong> ${jsonData.bio}</p>
+      <p><strong>Location:</strong> ${jsonData.location}</p>
+      <p><strong>Number of public repos:</strong> ${jsonData.public_repos}</p>
     </div>`;
     //Append the data 
-    divOverview.append(div);
-    console.log(json);
+    overView.append(div);
+    // console.log(jsonData);
 };
 
-//Function to fetch user repo data
+//Function to fetch user the repo data for the UL li
 const fetchRepos = async function () {
     const userRequest = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
     const repoData = await userRequest.json();
-
-    console.log(repoData);
+    // console.log(repoData);
     //Function callback to disp the repo list
-    dispRepoData(repoData);
+    dispRepoInfo(repoData);
+    filterRepos.classList.remove("hide");
 };
 //Callback to function to fetch the repo data
 fetchRepos();
-
-const dispRepoData = function (repo) {
-    dispRepoList.classList.remove("hide");
-    for (let title of repo) {
-        let repoLists = document.createElement("li");
+//Display list items of the UL
+const dispRepoInfo = function (repoData) {
+    repoList.classList.remove("hide");
+    for (let title of repoData) {
+        let repoLists = document.createElement("li"); //<==create list item
         repoLists.classList.add("filter-repos");
         repoLists.innerHTML = `<h3>${title.name}</h3>`
-        dispRepoList.append(repoLists);
+        repoList.append(repoLists);
     }
-
 };
 
-// Event listener for unordered list
-
-dispRepoList.addEventListener("click", function (e) {
+// Event listener for the unordered list
+repoList.addEventListener("click", function (e) {
     e.preventDefault();
-    // 
+    // check if the h3 was clicked
     if (e.target.matches("h3")) {
-
-        //   console.log(e.target.innerText);
-        const repo = e.target.innerText;
-        getSpecificRepo(repo);
-
+        const repoName = e.target.innerText;
+        // console.log(repoName); //<==test log the clicked li
+        getSpecificRepo(repoName); //<==callback to get repo info
     }
 });
-
-const getSpecificRepo = async function (repo) {
-
-    const userRequest = await fetch(`https://api.github.com/repos/${username}/${repo}`);
-    const repoInfo = await userRequest.json();
-
+//  function to get specific repo information
+const getSpecificRepo = async function (repoName) {
+    const repoRequest = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo = await repoRequest.json();
+    //fetch data from language_url property
     const fetchLanguages = await fetch(
-
-        repoInfo.languages_url//<==fetch from the repo the language info
+        repoInfo.languages_url //<==fetch from the repo the language info
     );
-
-    //Test log
-    console.log(fetchLanguages.url);
-
+    // save the JSON response
     const languageData = await fetchLanguages.json();
+    // console.log(repoInfo); //<==test log
 
-//Declare an empty array push the data from languageData into it
+    //Declare an empty array that will push the data from 
+    //languageData into the array
     const repoLanguages = [];
-    for (let elements in languageData){
+    for (let elements in languageData) {
         repoLanguages.push(elements);
-        console.log(repoLanguages);
-        
+        // console.log(repoLanguages); //<<==test log the array
     }
-
     // console.log(languageData);
-    dispSpecificRepoInfo(repoInfo, fetchLanguages);
-
-    
+    dispSpecificRepoInfo(repoInfo, repoLanguages);
 };
-
-
-
-
-
-const dispSpecificRepoInfo = function (repoData, languages) {
-
+// function to get specific repo information
+const dispSpecificRepoInfo = function (repoInfo, languages) {
+    // empty the HTML of the section with a class of “repo-data”
     repoData.innerHTML = "";
-    const div = document.createElement("div");
-   
+    //create a div to append the innerHTML
+    const infoRepodiv = document.createElement("div");
+    repoData.innerHTML =
+        `<h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${repoInfo.url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
     
+    repoData.append(infoRepodiv);
+    // console.log(repoData.innerHTML);
+
+     repoList.classList.add("hide");
+     repoData.classList.remove("hide");
+     filterRepos.classList.add("hide");
+     viewRepoButton.classList.remove("hide");
 };
 
+viewRepoButton.addEventListener("click",function(){
+
+    repoList.classList.remove("hide");
+    repoData.classList.add("hide");
+    filterRepos.classList.remove("hide");
+    viewRepoButton.classList.add("hide");
+
+
+
+});
